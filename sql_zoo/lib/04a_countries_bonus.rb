17a2f154b62b38a56c2577587b80881078d0312a
nllevin <50681582+nllevin@users.noreply.github.com>
2019-08-19 09:28:11 -0400

    Complete sql_zoo bonus problems

--- sql_zoo/lib/04a_countries_bonus.rb
+++ sql_zoo/lib/04a_countries_bonus.rb
@@ -17,6 +17,21 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries
+    WHERE
+      gdp > (
+        SELECT
+          MAX(gdp)
+        FROM
+          countries
+        WHERE
+          continent = 'Europe'
+        GROUP BY
+          continent
+      );
   SQL
 end
 
@@ -24,6 +39,21 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+    SELECT
+      largest.continent, 
+      largest.name, 
+      largest.area
+    FROM
+      countries AS largest
+    INNER JOIN (
+      SELECT
+        continent, MAX(area) AS max_area
+      FROM
+        countries
+      GROUP BY
+        continent
+    ) AS largest_by_continent ON (largest.area = largest_by_continent.max_area
+    AND largest.continent = largest_by_continent.continent)
   SQL
 end
 
@@ -31,5 +61,35 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+    SELECT
+      large_neighbor.name, large_neighbor.continent
+    FROM
+      countries AS large_neighbor
+    INNER JOIN (
+      SELECT
+        continent, MAX(population) AS second_largest_pop
+      FROM
+        countries
+      WHERE
+        name NOT IN (
+          SELECT
+            largest_countries.name
+          FROM
+            countries AS largest_countries
+          INNER JOIN (
+            SELECT
+              continent, MAX(population) AS largest_pop
+            FROM
+              countries
+            GROUP BY
+              continent
+          ) AS largest_by_continent ON largest_countries.continent = largest_by_continent.continent
+          AND largest_countries.population = largest_by_continent.largest_pop
+        )
+      GROUP BY
+        continent
+    ) AS second_largest_neighbor ON large_neighbor.continent = second_largest_neighbor.continent
+    WHERE
+      large_neighbor.population > 3 * second_largest_neighbor.second_largest_pop
   SQL
 end
--- sql_zoo/lib/07a_joins_bonus.rb
+++ sql_zoo/lib/07a_joins_bonus.rb
@@ -26,18 +26,42 @@ require_relative './sqlzoo.rb'
 def alison_artist
   # Select the name of the artist who recorded the song 'Alison'.
   execute(<<-SQL)
+    SELECT
+      albums.artist
+    FROM
+      albums
+    INNER JOIN
+      tracks ON albums.asin = tracks.album
+    WHERE 
+      tracks.song = 'Alison'
   SQL
 end
 
 def exodus_artist
   # Select the name of the artist who recorded the song 'Exodus'.
   execute(<<-SQL)
+    SELECT
+      albums.artist
+    FROM
+      albums
+    INNER JOIN
+      tracks ON albums.asin = tracks.album
+    WHERE
+      tracks.song = 'Exodus'
   SQL
 end
 
 def blur_songs
   # Select the `song` for each `track` on the album `Blur`.
   execute(<<-SQL)
+    SELECT
+      song
+    FROM
+      tracks
+    INNER JOIN
+      albums ON albums.asin = tracks.album
+    WHERE
+      albums.title = 'Blur'
   SQL
 end
 
@@ -46,6 +70,20 @@ def heart_tracks
   # the word 'Heart' (albums with no such tracks need not be shown). Order first by
   # the number of such tracks, then by album title.
   execute(<<-SQL)
+    SELECT
+      albums.title, COUNT(*) AS heart_count
+    FROM
+      albums
+    INNER JOIN
+      tracks ON albums.asin = tracks.album
+    WHERE
+      tracks.song LIKE '%Heart%'
+    GROUP BY
+      albums.title
+    HAVING
+      COUNT(*) > 0
+    ORDER BY
+      heart_count DESC, albums.title ASC
   SQL
 end
 
@@ -53,6 +91,14 @@ def title_tracks
   # A 'title track' has a `song` that is the same as its album's `title`. Select
   # the names of all the title tracks.
   execute(<<-SQL)
+    SELECT
+      tracks.song
+    FROM
+      tracks
+    INNER JOIN
+      albums ON tracks.album = albums.asin
+    WHERE
+      tracks.song = albums.title
   SQL
 end
 
@@ -60,6 +106,12 @@ def eponymous_albums
   # An 'eponymous album' has a `title` that is the same as its recording
   # artist's name. Select the titles of all the eponymous albums.
   execute(<<-SQL)
+    SELECT
+      title
+    FROM
+      albums
+    WHERE
+      title = artist
   SQL
 end
 
@@ -67,6 +119,18 @@ def song_title_counts
   # Select the song names that appear on more than two albums. Also select the
   # COUNT of times they show up.
   execute(<<-SQL)
+    SELECT
+      song, COUNT(*)
+    FROM (
+      SELECT DISTINCT
+        album, song
+      FROM
+        tracks
+    ) AS distinct_tracks
+    GROUP BY
+      song
+    HAVING
+      COUNT(*) > 2
   SQL
 end
 
@@ -75,6 +139,20 @@ def best_value
   # pence. Find the good value albums - show the title, the price and the number
   # of tracks.
   execute(<<-SQL)
+    SELECT
+      albums.title, albums.price, track_counts.num_tracks
+    FROM
+      albums
+    INNER JOIN (
+      SELECT
+        album, COUNT(*) AS num_tracks
+      FROM
+        tracks
+      GROUP BY
+        album
+    ) AS track_counts ON albums.asin = track_counts.album
+    WHERE
+      albums.price / track_counts.num_tracks < 0.5
   SQL
 end
 
@@ -83,6 +161,22 @@ def top_track_counts
   # tracks. List the top 10 albums. Select both the album title and the track
   # count, and order by both track count and title (descending).
   execute(<<-SQL)
+    SELECT
+      albums.title, track_counts.num_tracks
+    FROM
+      albums
+    INNER JOIN (
+      SELECT
+        album, COUNT(*) AS num_tracks
+      FROM
+        tracks
+      GROUP BY
+        album
+    ) AS track_counts ON albums.asin = track_counts.album
+    ORDER BY
+      track_counts.num_tracks DESC, albums.title DESC
+    LIMIT
+      10;
   SQL
 end
 
@@ -90,6 +184,24 @@ def rock_superstars
   # Select the artist who has recorded the most rock albums, as well as the
   # number of albums. HINT: use LIKE '%Rock%' in your query.
   execute(<<-SQL)
+    SELECT
+      artist, COUNT(*)
+    FROM (
+      SELECT DISTINCT
+        asin, artist
+      FROM
+        albums
+      INNER JOIN
+        styles ON albums.asin = styles.album
+      WHERE
+        style LIKE '%Rock%'
+    ) AS rock_albums
+    GROUP BY
+      artist
+    ORDER BY
+      COUNT(*) DESC
+    LIMIT
+      1;
   SQL
 end
 
@@ -102,5 +214,31 @@ def expensive_tastes
   # subquery. Next, JOIN the styles table to this result and use aggregates to
   # determine the average price per track.
   execute(<<-SQL)
+    SELECT
+      styles.style, (SUM(per_album_info.price) / SUM(per_album_info.album_num_tracks)) AS avg_style_track_price
+    FROM
+      styles
+    INNER JOIN(
+      SELECT
+        asin, price, track_counts.num_tracks AS album_num_tracks
+      FROM
+        albums
+      INNER JOIN(
+        SELECT
+          album, COUNT(*) AS num_tracks
+        FROM
+          tracks
+        GROUP BY
+          album
+      ) AS track_counts ON albums.asin = track_counts.album
+      WHERE
+        price IS NOT NULL
+    ) AS per_album_info ON styles.album = per_album_info.asin
+    GROUP BY
+      styles.style
+    ORDER BY
+      avg_style_track_price DESC, styles.style ASC
+    LIMIT
+      5
   SQL
 end
